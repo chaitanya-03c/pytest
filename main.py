093e98f85e5c15dd55857af80caa8dabd0426ed0
chaitanya-03c <67599890+chaitanya-03c@users.noreply.github.com>
2023-08-31 11:29:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import json
+import time
+
+
+VALID_STATES = ["IDLE", "WAIT", "IN_PROGRESS", "DONE"]
+VALID_EVENTS = ["READY_TO_WAIT", "STARTING", "COMPLETE"]
+
+def update_state(current_state, event):
+    if event == "READY_TO_WAIT" and current_state == "IDLE":
+        return "WAIT"
+    elif event == "STARTING" and current_state == "IDLE":
+        return "IDLE"
+    elif event == "COMPLETE" and current_state == "IN_PROGRESS":
+        return "DONE"
+    return current_state
+
+def main():
+    current_state = "IDLE"
+    
+    while current_state != "DONE":
+        try:
+            with open("events.json", "r") as events_file:
+                events_data = json.load(events_file)
+                event = events_data.get("event")
+                
+                if event in VALID_EVENTS:
+                    new_state = update_state(current_state, event)
+                    if new_state != current_state:
+                        current_state = new_state
+                        with open("state.txt", "w") as state_file:
+                            state_file.write(current_state)
+                            print(f"State updated: {current_state}")
+                    else:
+                        print(f"No state change: {current_state}")
+                else:
+                    print(f"Invalid event: {event}")
+                
+                time.sleep(1) 
+            
+        except FileNotFoundError:
+            print("events.json not found. please create json file...")
+            time.sleep(1)
+
+if __name__ == "__main__":
+    main()
